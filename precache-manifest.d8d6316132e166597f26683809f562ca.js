1b7821a3ace33ee284ae1fba740657017d6d53a3
sonmj426 <sonmj426@gmail.com>
2020-03-15 18:46:33 +0900

    Updates

---
+++ precache-manifest.d8d6316132e166597f26683809f562ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1638474b6841a04d15ddf33c42816c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "58cf77f4b1fabc283bff",
+    "url": "/movie_app/static/css/main.8b284cc7.chunk.css"
+  },
+  {
+    "revision": "a62fb7475389b46c3198",
+    "url": "/movie_app/static/js/2.50527ee9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.50527ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cf77f4b1fabc283bff",
+    "url": "/movie_app/static/js/main.2839ddca.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
